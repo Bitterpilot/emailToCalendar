777546f4f36ab798b42e4340109b6220f52e2b72
Nathan Minissale <nathanminissale@gmail.com>
2018-12-26 19:01:42 +0800

    Initial structs

---
+++ shift/shift.go
@@ -0,0 +1,26 @@
+package main
+
+type tableData struct {
+	day        string
+	date       string
+	startWork  string // gcal api expects strings
+	endWork    string // https://developers.google.com/calendar/create-events#metadata
+	totalHours string
+	breaks     string
+	pay        string
+	orgLevel   string
+}
+
+type shift struct {
+	summary        string // will be the procesed orgLevel (remove everything between \ inclusive)
+	location       string // Derived from orgLevel (if item before \ = AAAA then 303 if item before \ = BBBB)
+	eventDateStart string // date + startWork
+	eventDateEnd   string // date + endWork
+	processed      bool   // true/false/nil
+}
+
+// https://developers.google.com/calendar/extended-properties
+
+func main() {
+
+}
