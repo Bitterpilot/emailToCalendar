c0a0522d7eb8510bece37c1eaf58dd3d8ec66dbf
Nathan Minissale <nathanminissale@gmail.com>
2019-02-15 21:12:54 +0800

    Started copying methods from old code
    Calling from a temporary main in cmd/cli/main.go
    Also brought in a config file for temporary variables.

--- email/usecase.go
+++ email/usecase.go
@@ -6,8 +6,10 @@ import (
 
 type Usecase interface {
 	ListEmails(user string) []*models.Email
+	GetEmail(user string, msg *models.Email) *models.Email
 }
 
 type External interface {
 	ListEmails(user string) []*models.Email
+	GetEmail(user string, msg *models.Email) *models.Email
 }
