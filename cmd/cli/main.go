0c1eeeb87a68a72b5330de5a6ed0c5ce77a0f258
Nathan Minissale <nathanminissale@gmail.com>
2019-02-20 20:51:02 +0800

    Changed email entity to store a string in base64
    it seems that it will be easier to transfer and store the base64 and decode it where it is necessary

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 
 	"github.com/spf13/viper"
@@ -18,7 +19,7 @@ func main() {
 	user := viper.GetString(`user`)
 
 	usecase := external.NewGmailSrv(user)
-	listEmails := email.Usecase.ListEmails(usecase, user)
+	listEmails := email.External.ListEmails(usecase, user)
 	// for _, eml := range listEmails {
 	// 	fmt.Println(eml.MsgID, eml.ThdID)
 	// }
@@ -27,4 +28,10 @@ func main() {
 	// msg := listEmails[2]
 	body := email.External.GetEmail(usecase, user, msg).Body
 	fmt.Printf("%s\n", body)
+	fmt.Println()
+	d, err := base64.URLEncoding.DecodeString(body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s\n", d)
 }
--- pkg/email/usecase/emailusecase.go
+++ pkg/email/usecase/emailusecase.go
@@ -1,26 +1,62 @@
 package usecase
 
 import (
-	"github.com/bitterpilot/emailToCalendar/pkg/email"
-	"github.com/bitterpilot/emailToCalendar/pkg/models"
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+	"time"
+
+	"golang.org/x/net/html"
 )
 
-type emailUsecase struct {
-	emailExternal email.External
-}
+func readTag(body, readTag, endTag string) (table []string) {
+	tokenizer := html.NewTokenizer(strings.NewReader(body))
+	content := []string{}
 
-// NewEmailUsecase ...
-func NewEmailUsecase(e email.External) email.Usecase {
-	return &emailUsecase{
-		emailExternal: e,
+	// While have not hit the </endTag> tag
+	for tokenizer.Token().Data != endTag {
+		tocNext := tokenizer.Next()
+		if tocNext == html.StartTagToken {
+			token := tokenizer.Token()
+			if token.Data == readTag {
+				inner := tokenizer.Next()
+				if inner == html.TextToken {
+					text := strings.TrimSpace((string)(tokenizer.Text()))
+					content = append(content, text)
+				}
+			}
+		}
 	}
+	return content
 }
 
-func (e *emailUsecase) GetEmail(user string, msg *models.Email) *models.Email {
-	return nil
-}
+// readYear originally returned years as 2 digit ie,[18 19] so it could be used
+// more easily in the date format RFC822Z. ie("02 Jan 06 15:04 -0700").
+// it now returns a map with time.Time to allow for easier manipulation later.
+func readYear(eml string) (map[string]time.Time, error) {
+	re := regexp.MustCompile(`([\d]{1,2} [\w]{3} [\d]{4})`)
+	match := re.FindAllString(eml, -1) // FindAllString(t,n) n = max number of matches
+	// verify that there is only two dates
+	if len(match) != 2 {
+		msg := fmt.Sprintf("Cannot parse body text to get roster date range\n Expected 2 got %d", len(match))
+		return nil, errors.New(msg)
+	}
 
-func (e *emailUsecase) ListEmails(user string) []*models.Email {
-	item := e.emailExternal.ListEmails(user)
-	return item
+	var dates map[string]time.Time
+	loc, _ := time.LoadLocation("Australia/Perth")
+	for i, val := range match {
+		layout := "2 Jan 2006"
+		ti, err := time.ParseInLocation(layout, val, loc)
+		if err != nil {
+			fmt.Println(err)
+		}
+		switch {
+		case i == 0:
+			dates["Start"] = ti
+		case i == 1:
+			dates["End"] = ti
+		}
+	}
+	return dates, nil
 }
