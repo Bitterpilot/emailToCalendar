054bc9c6162ee8ae26a46dbe65559fdd96c5a473
Nathan Minissale <nathanminissale@gmail.com>
2019-02-20 20:47:15 +0800

    updates to the old db package
    removed sql commands create statments
    
    added a comment to a mongodb as a hint for where i want this to go later down the line

--- pkg/db/emails.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS "emails";
-CREATE TABLE emails (
-		ID INTEGER NOT NULL PRIMARY KEY,
-		msgID text,
-		thdID text,
-		timeRecieved text,
-		proccessed BOOLEAN DEFAULT 0,
-		error BOOLEAN DEFAULT 0
-	);
