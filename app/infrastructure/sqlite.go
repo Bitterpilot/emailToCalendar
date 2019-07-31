191c1cf9a39fad35865acad2240a2bc3b3db312b
Nathan Minissale <nathan@Nathans-iMac.local>
2019-07-31 11:52:42 +0800

    Moved DB commands

--- app/infrastructure/sqlite.go
+++ app/infrastructure/sqlite.go
@@ -3,8 +3,7 @@ package infrastructure
 import (
 	"database/sql"
 
-	log "github.com/sirupsen/logrus"
-
+	"github.com/bitterpilot/emailToCalendar/app/infrastructure/sqlite"
 	"github.com/bitterpilot/emailToCalendar/models"
 )
 
@@ -20,53 +19,10 @@ func NewDB(db *sql.DB) *Store {
 
 // ListByMsgID
 func (s Store) ListByMsgID(msgID string) (string, error) {
-	tx, err := s.db.Begin()
-	if err != nil {
-		return "", err
-	}
-	defer tx.Commit()
-
-	var msgList string
-
-	stmt, err := tx.Query("SELECT msgID FROM emails WHERE msgID = ?", msgID)
-	if err != nil {
-		return "", err
-	}
-	defer stmt.Close()
-
-	for stmt.Next() {
-		// for each row, scan the result into our composite object
-		err = stmt.Scan(&msgList)
-		if err != nil {
-			return "", err
-		}
-	}
-	return msgList, nil
+	return sqlite.SelectByMsgID(msgID, s.db)
 }
 
 // InsertEmail
 func (s Store) InsertEmail(e models.Email) (int, error) {
-	tx, err := s.db.Begin()
-	if err != nil {
-		return -1, err
-	}
-	defer tx.Commit()
-
-	stmt, err := tx.Prepare("INSERT INTO emails(msgID, thdID, timeRecieved) values(?,?,?)")
-	if err != nil {
-		return -1, err
-	}
-
-	res, err := stmt.Exec(e.MsgID, e.ThdID, e.TimeReceived)
-	if err != nil {
-		return -1, err
-	}
-
-	id, err := res.LastInsertId()
-	if err != nil {
-		return -1, err
-	}
-
-	log.WithFields(log.Fields{"Database ID": id}).Info("Inserted to DB.")
-	return int(id), nil
+	return sqlite.Insert(e, s.db)
 }
--- app/infrastructure/sqlite/database.go
+++ app/infrastructure/sqlite/database.go
@@ -1,2 +1,62 @@
 package sqlite
 
+import (
+	"database/sql"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/bitterpilot/emailToCalendar/models"
+)
+
+// SelectByMsgID
+func SelectByMsgID(msgID string, db *sql.DB) (string, error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return "", err
+	}
+	defer tx.Commit()
+
+	var msgList string
+
+	stmt, err := tx.Query("SELECT msgID FROM emails WHERE msgID = ?", msgID)
+	if err != nil {
+		return "", err
+	}
+	defer stmt.Close()
+
+	for stmt.Next() {
+		// for each row, scan the result into our composite object
+		err = stmt.Scan(&msgList)
+		if err != nil {
+			return "", err
+		}
+	}
+	return msgList, nil
+}
+
+// Insert
+func Insert(e models.Email, db *sql.DB) (int, error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return -1, err
+	}
+	defer tx.Commit()
+
+	stmt, err := tx.Prepare("INSERT INTO emails(msgID, thdID, timeRecieved) values(?,?,?)")
+	if err != nil {
+		return -1, err
+	}
+
+	res, err := stmt.Exec(e.MsgID, e.ThdID, e.TimeReceived)
+	if err != nil {
+		return -1, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return -1, err
+	}
+
+	log.WithFields(log.Fields{"Database ID": id}).Info("Inserted to DB.")
+	return int(id), nil
+}
