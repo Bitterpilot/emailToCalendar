07459c3b125c5979a40bf85b427f471ec10c8b0c
Nathan Minissale <nathan@Nathans-iMac.local>
2019-08-05 12:56:46 +0800

    Updated provider signature to be more clear about what type of provider it is

--- app/infrastructure/gmail.go
+++ app/infrastructure/gmail.go
@@ -6,24 +6,24 @@ import (
 	"google.golang.org/api/gmail/v1"
 )
 
-// Provider holds objects and functions for gmail API calls
-type Provider struct {
+// EmailProvider holds objects and functions for gmail API calls
+type EmailProvider struct {
 	service *gmail.Service
 	user    string
 }
 
 
-// NewGmailProvider creates a new Provider
-func NewGmailProvider(u string) *Provider {
+// NewEmailProvider creates a new Provider
+func NewEmailProvider(u string) *EmailProvider {
 	s := gmailgetter.NewService()
-	return &Provider{
+	return &EmailProvider{
 		service: s,
 		user:    u,
 	}
 }
 
 // List converts a gmail message type to a model.Email type
-func (p Provider) List(labelIDs, sender, subject string) []models.Email {
+func (p EmailProvider) List(labelIDs, sender, subject string) []models.Email {
 	list := gmailgetter.ListMessages(p.user, labelIDs, sender, subject)
 	var ret []models.Email
 	for _, gmsg := range list {
@@ -37,7 +37,7 @@ func (p Provider) List(labelIDs, sender, subject string) []models.Email {
 }
 
 // Get a body and date from a gmail message type and converts to a model.Email type
-func (p Provider) Get(e models.Email) models.Email {
+func (p EmailProvider) Get(e models.Email) models.Email {
 	InternalDate, emailBody := gmailgetter.GetMessage(p.user, e.MsgID)
 	e.TimeReceived = InternalDate
 	e.Body = emailBody
